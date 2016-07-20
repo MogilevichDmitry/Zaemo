9870113e7912cf6475884de2504a2bfc89eed174
MogilevichDmitry <dmsmain@gmail.com>
2016-07-20 12:26:22 +0300

    Default project settings withour redux and css

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import greeter from './Greeter.js';
+
+document.getElementById('app').appendChild(greeter());
+
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry:  __dirname + "/src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel' },
+    ]
+  },
+
+  devServer: {
+    contentBase: "./dist",
+    colors: true,
+    historyApiFallback: true,
+    inline: true
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+}
