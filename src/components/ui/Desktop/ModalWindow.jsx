28e3830acbf56341d64e3b5707802ed74bc61153
MogilevichDmitry <dmsmain@gmail.com>
2016-07-26 13:42:29 +0300

    Refactoring modal-window

--- src/components/ui/Desktop/ModalWindow.jsx
+++ src/components/ui/Desktop/ModalWindow.jsx
@@ -1,22 +1,45 @@
 import React from 'react';
 import Radium from 'radium';
 import AppBar from 'material-ui/AppBar';
+import MenuItem from 'material-ui/MenuItem';
+import SelectField from 'material-ui/SelectField';
+import Dialog from 'material-ui/Dialog';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import IconButton from 'material-ui/IconButton';
 
-const ModalWindow =({ children, title, width, height }) => {
-  return <div style={{width: width, height: height}}>
-    <AppBar
-      title={title}
-      showMenuIconButton={false}
-      iconElementRight={
+const ModalWindow =({ children, title }) => {
+  return <div>
+    <Dialog
+      modal={true}
+      open={true}
+      style={styles.dialog}
+    >
+      <AppBar
+        title={title}
+        showMenuIconButton={false}
+        style={styles.appBar}
+        iconElementRight={
         <IconButton><NavigationClose /></IconButton>
       }
-    />
-    <div>
-      {children}
-    </div>
+      />
+      <div>
+        {children}
+      </div>
+    </Dialog>
+
   </div>;
 }
+
+const styles = {
+  dialog: {
+    position: 'relative',
+  },
+  appBar: {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    right: '0',
+  }
+};
 
 export default Radium(ModalWindow);
