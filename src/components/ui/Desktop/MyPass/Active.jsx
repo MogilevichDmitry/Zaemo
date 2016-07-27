12c074cce84c081d5c1c2c84b36a75e6d5312855
MogilevichDmitry <dmsmain@gmail.com>
2016-07-27 13:54:52 +0300

    Add style to gym status Active

--- src/components/ui/Desktop/MyPass/Active.jsx
+++ src/components/ui/Desktop/MyPass/Active.jsx
@@ -13,24 +13,68 @@ const expiresIn = {
 
 const Active = () => {
   return <ModalWindow title="Active pass" open={true}>
-    <GymPass
-      src={gym}
-      title="GymABC"
-      status="active"
-    />
+    <div style={styles.wrapper}>
+      <GymPass
+        src={gym}
+        title="GymABC"
+        status="active"
+      />
 
-    <div>
-      <div>
-        While countdown is active you have full access
-        to the gym facilities and classes.
-      </div>
+      <div style={styles.countDown}>
+        <div style={styles.countDownDescription}>
+          While countdown is active you have full access
+          to the gym facilities and classes.
+        </div>
 
-      <div>
-        <div>Expires in:</div>
-        <div>{`${expiresIn.hours}h ${expiresIn.minutes}m ${expiresIn.seconds}s`}</div>
+        <div style={styles.countDownTimer}>
+          <div style={styles.countDownTitle}>Expires in:</div>
+          <span style={styles.countDownValue}>{expiresIn.hours}</span>
+          <span style={styles.countDownTimeHint}>h</span>
+          <span style={styles.countDownValue}>{expiresIn.minutes}</span>
+          <span style={styles.countDownTimeHint}>m</span>
+          <span style={styles.countDownValue}>{expiresIn.seconds}</span>
+          <span style={styles.countDownTimeHint}>s</span>
+
+        </div>
       </div>
     </div>
   </ModalWindow>;
-}
+};
+
+const styles = {
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  countDown: {
+    fontSize: '13px',
+    color: '#999999',
+    flex: '0.5',
+    margin: 'auto',
+    maxWidth: '300px',
+    textAlign: 'center',
+  },
+  countDownDescription: {
+    marginBottom: '15px',
+  },
+  countDownTimer: {
+    color: 'black',
+    fontSize: '18px',
+  },
+  countDownTitle: {
+    marginBottom: '10px',
+    fontWeight: '500',
+  },
+  countDownValue: {
+    fontWeight: '600',
+    fontSize: '24px',
+  },
+  countDownTimeHint: {
+    color: '#999999',
+    marginRight: '7px',
+    fontSize: '24px',
+  },
+};
 
 export default Radium(Active);
