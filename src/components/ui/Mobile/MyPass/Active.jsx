6b7d0d22cb5a38add6f6fe8d50ccfc6e0fae673b
MogilevichDmitry <dmsmain@gmail.com>
2016-07-29 00:22:38 +0300

    Style statuses for MyPass mobile version component

--- src/components/ui/Mobile/MyPass/Active.jsx
+++ src/components/ui/Mobile/MyPass/Active.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Radium from 'radium';
 import GymPass from './GymPass.jsx';
 
-import gymA from '../../../../images/content/gymA.jpg';
+import background from '../../../../images/layout/myPassesBg.jpg';
+import gymA from '../../../../images/content/gymMyPass.jpg';
 
 const expiresIn = {
   hours: '23',
@@ -11,7 +12,7 @@ const expiresIn = {
 };
 
 const Active = () => {
-  return <div>
+  return <div style={styles.wrapper}>
     <GymPass
       src={gymA}
       barTitle="Active pass"
@@ -19,18 +20,65 @@ const Active = () => {
       status="active"
     />
 
-    <div>
-      <div>
+    <div style={styles.countDown}>
+      <div style={styles.countDownDescription}>
         While countdown is active you have full access
         to the gym facilities and classes.
       </div>
 
-      <div>
-        <div>Expires in:</div>
-        <div>{`${expiresIn.hours}h ${expiresIn.minutes}m ${expiresIn.seconds}s`}</div>
+      <div style={styles.countDownTimer}>
+        <div style={styles.countDownTitle}>Expires in:</div>
+        <span style={styles.countDownValue}>{expiresIn.hours}</span>
+        <span style={styles.countDownTimeHint}>h</span>
+        <span style={styles.countDownValue}>{expiresIn.minutes}</span>
+        <span style={styles.countDownTimeHint}>m</span>
+        <span style={styles.countDownValue}>{expiresIn.seconds}</span>
+        <span style={styles.countDownTimeHint}>s</span>
+
       </div>
     </div>
   </div>;
-}
+};
+
+const styles = {
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    backgroundImage: `url(${background})`,
+    backgroundSize: 'cover',
+    minHeight: '100vh',
+    fontFamily: 'Ubuntu',
+    paddingBottom: '25px',
+  },
+  countDown: {
+    fontSize: '13px',
+    color: '#cccccc',
+    margin: '0 auto',
+    maxWidth: '350px',
+    textAlign: 'center',
+    padding: '0 30px',
+  },
+  countDownDescription: {
+    marginBottom: '15px',
+  },
+  countDownTimer: {
+    fontSize: '18px',
+  },
+  countDownTitle: {
+    marginBottom: '10px',
+    fontWeight: '500',
+    color: 'white',
+  },
+  countDownValue: {
+    fontWeight: '600',
+    fontSize: '24px',
+    color: 'white',
+  },
+  countDownTimeHint: {
+    marginRight: '7px',
+    fontSize: '24px',
+  },
+};
 
 export default Radium(Active);
--- src/components/ui/Mobile/MyPass/Expired.jsx
+++ src/components/ui/Mobile/MyPass/Expired.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Radium from 'radium';
 import GymPass from './GymPass.jsx';
-import FlagButton from 'material-ui/FlatButton';
+import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 
-import gymA from '../../../../images/content/gymA.jpg';
+import background from '../../../../images/layout/myPassesBg.jpg';
+import gymA from '../../../../images/content/gymMyPass.jpg';
 
 const Expired = () => {
-  return <div>
+  return <div style={styles.wrapper}>
     <GymPass
       src={gymA}
       barTitle="Active pass"
@@ -15,26 +16,91 @@ const Expired = () => {
       status="Expired 2 days ago"
     />
 
-    <div>
-      <RaisedButton label="Purchase Again" primary={true} />
+    <div style={styles.statusWrapper}>
+      <div style={styles.buttonBox}>
+        <RaisedButton label="Purchase Again" primary={true} fullWidth={true} />
+      </div>
 
       <div>
-        <h1>Other available passes</h1>
+        <div style={styles.statusTitle}>Other available passes</div>
 
-        <div>
-          <div>Month pass</div>
-          <div>Price: $50.00 dlls</div>
-          <FlagButton label="Select" primary={true} />
+        <div style={styles.passSelect}>
+          <div>
+            <div style={styles.passSelectTitle}>Month pass</div>
+            <div>
+              <span>Price: </span>
+              <span style={styles.passSelectCost}>$50.00 dlls</span>
+            </div>
+          </div>
+          <FlatButton
+            label="select"
+            primary={true}
+            style={styles.passSelectButton}
+          />
         </div>
-
-        <div>
-          <div>Annual pass</div>
-          <div>Price: $50.00 dlls</div>
-          <FlagButton label="Select" primary={true} />
+        <div style={styles.passSelect}>
+          <div>
+            <div style={styles.passSelectTitle}>Annual pass</div>
+            <div>
+              <span>Price: </span>
+              <span style={styles.passSelectCost}>$50.00 dlls</span>
+            </div>
+          </div>
+          <FlatButton
+            label="select"
+            primary={true}
+            style={styles.passSelectButton}
+          />
         </div>
       </div>
     </div>
   </div>;
-}
+};
+
+const styles = {
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    backgroundImage: `url(${background})`,
+    backgroundSize: 'cover',
+    minHeight: '100vh',
+    fontFamily: 'Ubuntu',
+    paddingBottom: '25px',
+  },
+  statusWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    padding: '0 5px',
+    fontSize: '13px',
+    color: '#cccccc',
+  },
+  buttonBox: {
+    width: '60%',
+    margin: '0 auto 20px',
+  },
+  statusTitle: {
+    padding: '0 20px 15px',
+    borderBottom: '1px solid #9a918a',
+  },
+  passSelect: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '15px 20px',
+    borderBottom: '1px solid #9a918a',
+  },
+  passSelectTitle: {
+    margin: 'auto 0',
+    color: 'white',
+  },
+  passSelectCost: {
+    margin: 'auto 10px auto 2px',
+    color: 'white',
+  },
+  passSelectButton: {
+    margin: 'auto 0',
+  },
+};
 
 export default Radium(Expired);
